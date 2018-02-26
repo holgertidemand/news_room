579815adfd0fcd3f21d841ba6c872772737c94c9
tochman <thomas@craftacademy.se>
2018-02-26 11:22:19 +0100

    adds view articles feature scaffods basic step definitions

---
+++ features/step_definitions/application_steps.rb
@@ -0,0 +1,8 @@
+Given(/^I visit the site$/) do
+  visit root_path
+end
+
+Given("the following news articles exist") do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
