8de2734edcb5175b3f740373c45b1bd6f4ab455b
tochman <thomas@craftacademy.se>
2018-02-27 14:06:39 +0100

    introduces Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
+
+  def authenticate_user!
+    redirect_to new_user_registration_path, notice: 'You need to be logged in to access this content' unless current_user
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: :show
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
-    if !current_user
-      flash[:notice] = 'You need to be logged in to access this content'
-      redirect_to new_user_registration_path
-    elsif !current_user.subscriber?
+    begin
+      @article = Article.find(params[:id])
+      authorize @article
+    rescue Pundit::NotAuthorizedError
       flash[:notice] = 'You need to be a subscriber to access this content'
       redirect_to new_subscription_path
-    else
-      render :show
     end
   end
 end
